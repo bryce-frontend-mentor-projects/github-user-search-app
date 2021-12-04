4e418ff12643f20c075d9c633f83f8c1a1e333bf
vbfischer <bryce@berzerker-soft.com>
2021-12-04 17:58:47 +0000

    Deploying to gh-pages from @ bryce-frontend-mentor-projects/github-user-search-app@572133d2809374c59fcb45f34172315048233493 🚀

--- components/info.d.ts
+++ components/info.d.ts
@@ -5,6 +5,7 @@ export interface InfoItemListProps {
 export interface InfoItemProps {
     icon: React.ReactNode;
     value: Nullable<string>;
+    url?: string;
 }
 export declare const InfoItem: (props: InfoItemProps) => JSX.Element;
 export declare const InfoItemList: (props: React.PropsWithChildren<InfoItemListProps>) => JSX.Element;
--- types.d.ts
+++ types.d.ts
@@ -1,2 +1,17 @@
 export declare type None = null | undefined;
 export declare type Nullable<T> = T | None;
+export interface User {
+    login: string;
+    "avatar_url": string;
+    url: string;
+    name: Nullable<string>;
+    company: Nullable<string>;
+    blog: string;
+    location: Nullable<string>;
+    bio: Nullable<string>;
+    "twitter_username": Nullable<string>;
+    "public_repos": number;
+    followers: number;
+    following: number;
+    "created_at": number;
+}
